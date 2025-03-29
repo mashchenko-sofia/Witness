8339a6c77af89839eecc0d5be5e660f7c072db90
Maschenko Sofia <m.sofia05@mail.ru>
2025-03-29 12:03:29 +0300

    last commit

--- js/index.js
+++ js/index.js
@@ -4,14 +4,22 @@ document.addEventListener("DOMContentLoaded", () => {
     
     // BUTTONS —————————————————————————————————————————————————————————
     const btnStart = document.querySelector(".btn-start");
+
     const btnCustomise = document.querySelector(".btn-customise");
     const btnInstall = document.querySelector(".btn-install");
     const btnTest = document.querySelector(".btn-test");
+
     const btnContinue = document.querySelector(".btn-continue");
     const btnFinish = document.querySelector(".btn-finish");
+
     const btnArrowLeft = document.querySelector(".btn-arrow-left");
     const btnArrowRight = document.querySelector(".btn-arrow-right");
+
     const btnCloseAlert = document.querySelector(".alert__btn-close");
+    const btnOpenSidebar = document.querySelector(".btn-open-sidebar");
+    const btnCloseSidebar = document.querySelector(".btn-close-sidebar");
+
+    const btnsChooseForm = document.querySelectorAll(".btn-choose-form");
 
     // SECTIONS —————————————————————————————————————————————————————————
     const secStart = document.querySelector(".start");
@@ -24,6 +32,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const secInfoText = document.querySelector(".info");
 
+    // DEVICE  ——————————————————————————————————————————————————
+    const deviceForms = document.querySelectorAll(".device__form");
+
     // ALERT ————————————————————————————————————————————————————————
     const alert = document.querySelector(".alert");
     const alertMessage = document.querySelector(".alert__message");
@@ -31,11 +42,11 @@ document.addEventListener("DOMContentLoaded", () => {
     // CURSOR ——————————————————————————————————————————————————
     const cursor = document.querySelector('.cursor');
 
-    const moveCursor = (e)=> {
+    const moveCursor = (e) => {
         const mouseY = e.clientY;
         const mouseX = e.clientX;
         cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-    }   
+    }
 
     window.addEventListener('mousemove', moveCursor);
 
@@ -46,24 +57,126 @@ document.addEventListener("DOMContentLoaded", () => {
     const chart4 = document.querySelector(".chart-4");
     const chart5 = document.querySelector(".chart-5");
 
-    // const animateChart1 = anime({
-    //     targets: '.chart1',
-    //     points: [
-    //         { value: [
-    //             '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369',
-    //             '70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369'
-    //         ]},
-    //         { value: '70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369' },
-    //         { value: '70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369' },
-    //         { value: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' }
-    //     ],
-    //     easing: 'easeOutQuad',
-    //     duration: 2000,
-    //     loop: true
-    // });
-    // animateChart1.play();
-
+    // CHART 1
+    anime({
+        targets: '#rect-1',
+        x: ['80' ,'0'],
+        y: ['0', '30'],
+        duration: 2000,
+        easing: 'easeInOutQuad',
+        direction: "alternate",
+        loop: true,
+    })
+    anime({
+        targets: '#circle-1',
+        cx: ['95' ,'15'],
+        cy: ['15', '45'],
+        duration: 2000,
+        easing: 'easeInOutQuad',
+        direction: "alternate",
+        loop: true,
+    })
+    anime({
+        targets: '#line-1',
+        x1: ['28' ,'18'],
+        y1: ['98', '138'],
+        x2: ['95' ,'15'],
+        y2: ['15', '45'],
+        duration: 2000,
+        easing: 'easeInOutQuad',
+        direction: "alternate",
+        loop: true,
+    })
+    anime({
+        targets: '#rect-2',
+        x: ['75' ,'85'],
+        y: ['80', '60'],
+        duration: 2000,
+        easing: 'easeInOutQuad',
+        direction: "alternate",
+        loop: true,
+    })
+    anime({
+        targets: '#circle-2',
+        cx: ['80' ,'90'],
+        cy: ['85', '65'],
+        duration: 2000,
+        easing: 'easeInOutQuad',
+        direction: "alternate",
+        loop: true,
+    })
+    anime({
+        targets: '#line-2',
+        x1: ['80' ,'90'],
+        y1: ['85', '65'],
+        x2: ['95' ,'15'],
+        y2: ['15', '45'],
+        duration: 2000,
+        easing: 'easeInOutQuad',
+        direction: "alternate",
+        loop: true,
+    })
+    anime({
+        targets: '#rect-3',
+        x: ['20' ,'10'],
+        y: ['90', '130'],
+        duration: 2000,
+        easing: 'easeInOutQuad',
+        direction: "alternate",
+        loop: true,
+    })
+    anime({
+        targets: '#circle-3',
+        cx: ['28' ,'18'],
+        cy: ['98', '138'],
+        duration: 2000,
+        easing: 'easeInOutQuad',
+        direction: "alternate",
+        loop: true,
+    })
+    anime({
+        targets: '#line-3',
+        x1: ['80' ,'90'],
+        y1: ['85', '65'],
+        x2: ['101' ,'111'],
+        y2: ['151' ,'161'],
+        duration: 2000,
+        easing: 'easeInOutQuad',
+        direction: "alternate",
+        loop: true,
+    })
+    anime({
+        targets: '#rect-4',
+        x: ['90' ,'100'],
+        y: ['140' ,'150'],
+        duration: 2000,
+        easing: 'easeInOutQuad',
+        direction: "alternate",
+        loop: true,
+    })
+    anime({
+        targets: '#circle-4',
+        cx: ['101' ,'111'],
+        cy: ['151' ,'161'],
+        duration: 2000,
+        easing: 'easeInOutQuad',
+        direction: "alternate",
+        loop: true,
+    })
+    anime({
+        targets: '#line-4',
+        x1: ['28' ,'18'],
+        y1: ['98', '138'],
+        x2: ['101' ,'111'],
+        y2: ['151' ,'161'],
+        duration: 2000,
+        easing: 'easeInOutQuad',
+        direction: "alternate",
+        loop: true,
+    })
     
+
+    // CHART 2
     for (let i = 0; i < 4; i++) {
         let el = document.createElement("div");
         el.classList.add("chart-2__el");
@@ -76,11 +189,8 @@ document.addEventListener("DOMContentLoaded", () => {
         anime({
             targets: '.chart-2__el-line',
             width: () => {return anime.random(10, 100) + '%'},
-            // translateY: () => { return anime.random(-500, 500) + '%'},
-            // duration: () => { return anime.random(300, 800)},
             duration: 500,
             easing: 'easeInOutQuad',
-            // delay: anime.stagger(50),
             complete: animateChart2,
         });
     }
@@ -89,7 +199,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-
+    // CHART 3
     for (let i = 0; i < 14; i++) {
         let el = document.createElement("div");
         el.classList.add("chart-3__el");
@@ -100,16 +210,26 @@ document.addEventListener("DOMContentLoaded", () => {
             targets: '.chart-3__el',
             translateY: () => { return anime.random(-500, 500) + '%'},
             duration: () => { return anime.random(300, 800)},
-            // duration: anime.stagger(100),
             easing: 'easeInOutQuad',
-            // delay: anime.stagger(15),
             complete: animateChart3,
         });
     }
     animateChart3();
 
 
+    // CHART 4
+    function animateChart4() {
+        anime({
+            targets: '.chart-4__el',
+            rotate: () => {return anime.random(0, 360)},
+            duration: () => {return anime.random(500, 800)},
+            easing: 'easeInOutQuad',
+            complete: animateChart4,
+        })
+    }
+    animateChart4();
 
+    // CHART 5
     for (let i = 0; i < 24; i++) {
         let el = document.createElement("div");
         el.classList.add("chart-5__el");
@@ -119,21 +239,106 @@ document.addEventListener("DOMContentLoaded", () => {
         anime({
             targets: '.chart-5__el',
             height: () => {return anime.random(10, 180)},
-            // height: [
-            //     {value: () => {return anime.random(10, 90)}}
-            //     ,
-            //     {value: () => {return anime.random(100, 180)}},
-            // ],
-            // height: 180,
-            // duration: 500,
             duration: () => {return anime.random(200, 500)},
             easing: 'easeInOutQuad',
-            // direction: "alternate",
-            // delay: anime.stagger(15),
+            delay: anime.stagger(10),
             complete: animateChart5,
         });
     }
     animateChart5();
+
+
+
+    // PROCESSOR ——————————————————————————————————————————————————
+    const processorPartsWrapper = document.querySelector('.processor-parts');
+    const processorParts = document.querySelectorAll('.processor-part');
+    const processorBox = document.querySelector('.processor-box');
+    const boxParts = document.querySelectorAll('.box-part');
+
+    processorParts.forEach((part, index) => {
+        let isGrabed = false;
+        let offsetX;
+        let offsetY;
+
+        part.addEventListener('mousedown', (e) => {
+            isGrabed = true;
+
+            offsetX = e.clientX - part.getBoundingClientRect().left;
+            offsetY = e.clientY - part.getBoundingClientRect().top;
+
+            document.body.append(part);
+            part.style.zIndex = 1000;
+        });
+        document.addEventListener('mousemove', (e) => {
+            if (isGrabed) {
+                part.hidden = true;
+                let elemBelow = document.elementFromPoint(e.clientX, e.clientY);
+                part.hidden = false;
+
+                if (elemBelow == boxParts[index]) {
+                    processorParts[index].classList.add('hidden');
+                    boxParts[index].style.fillOpacity = '1';
+                }
+
+
+
+
+                part.style.left = e.clientX - offsetX + 'px';
+                part.style.top = e.clientY - offsetY + 'px';
+
+                part.style.scale = '0.5';
+            }
+        })
+        document.addEventListener('mouseup', () => {
+            isGrabed = false;
+            part.style.scale = '1';
+            processorPartsWrapper.append(part);
+        });
+        part.ondragstart = () => {
+            return false;
+        };
+    })
+    processorBox.ondragstart = () => {
+        return false;
+    };
+
+
+
+    // FIELD ——————————————————————————————————————————————————
+    const field = document.querySelector('.field');
+    const fieldWidth = 12;
+    const fieldHeight = 12;
+    const fieldGrid = [fieldWidth, fieldHeight];
+    const numberOfDots = fieldWidth * fieldHeight; 
+
+    for (let i = 0; i < numberOfDots; i++) {
+        const dot = document.createElement('div');
+        dot.classList.add('dot');
+        dot.id = i;
+        field.appendChild(dot);
+
+        dot.addEventListener('click', (e) => {
+            anime({
+                targets: ".dot",
+                scale: [
+                    {value: 1.35, easing: "easeOutSine", duration: 150},
+                    {value: 1, easing: "easeInOutQuad", duration: 300},
+                ],
+                translateY: [
+                    {value: -15, easing: "easeOutSine", duration: 150},
+                    {value: 0, easing: "easeInOutQuad", duration: 300},
+                ],
+                opacity: [
+                    { value: 0.5, easing: "easeInOutQuad", duration: 150 },
+                    { value: 1, easing: "easeOutSine", duration: 300 },
+                ],
+                delay: anime.stagger(50, {
+                    grid: fieldGrid,
+                    from: e.target.id,
+                })
+            })
+        })
+    }
 
 
     // ANIMATIONS —————————————————————————————————————————————————————
@@ -159,15 +364,30 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     const animateAlert = anime({
         targets: '.alert',
-        // loop: false,
         autoplay: false,
-        duration: 2000,
+        duration: 1200,
         opacity: 1,
-        translateY: 600,
+        translateY: '450%',
         scale: 2,
         easing: 'easeInOutQuad',
         direction: 'alternate',
-        endDelay: 1500,
+        endDelay: 750,
+    });
+    const animateBtn = anime({
+        targets: '.btn',
+        autoplay: false,
+        translateX: '100%',
+        duration: 1000,
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+    });
+    const animateSidebar = anime({
+        targets: '.sidebar',
+        loop: false,
+        autoplay: false,
+        duration: 2000,
+        easing: "easeInOutQuad",
+        translateX: '100%',
     });
 
     // FUNCTIONS —————————————————————————————————————————————————————————
@@ -233,44 +453,63 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     btnCustomise.addEventListener("click", () => {
-        secMenu.classList.add("hidden");
-        secCustomise.classList.remove("hidden");
-        secInfoText.textContent = "customise|01";
+        animateBtn.play();
 
-        btnContinue.classList.remove("hidden");
-        showAlert("Customise form, color and processor!")
+        setTimeout(() => {
+            secMenu.classList.add("hidden");
+            secCustomise.classList.remove("hidden");
+            secInfoText.textContent = "customise|01";
+
+            btnContinue.classList.remove("hidden");
+            showAlert("Customise form, color and processor!")
+        }, 1000);
     });
     btnInstall.addEventListener("click", () => {
-        secMenu.classList.add("hidden");
-        secInstall.classList.remove("hidden");
-        secInfoText.textContent = "install|02";
+        animateBtn.play();
 
-        btnContinue.classList.remove("hidden");
-        showAlert("Install all parts of the processor!");
+        setTimeout(() => {
+            secMenu.classList.add("hidden");
+            secInstall.classList.remove("hidden");
+            secInfoText.textContent = "install|02";
+    
+            btnContinue.classList.remove("hidden");
+            showAlert("Install all parts of the processor!");
+        }, 1000);
     });
     btnTest.addEventListener("click", () => {
-        secMenu.classList.add("hidden");
-        secTest.classList.remove("hidden");
-        secInfoText.textContent = "test|03";
+        animateBtn.play();
 
-        btnFinish.classList.remove("hidden");
-        showAlert("Test if the device responds to noise!");
+        setTimeout(() => {
+            secMenu.classList.add("hidden");
+            secTest.classList.remove("hidden");
+            secInfoText.textContent = "test|03";
+
+            btnFinish.classList.remove("hidden");
+            showAlert("Test if the device responds to noise!");
+        }, 1000);
     });
 
 
 
     btnContinue.addEventListener("click", () => {
-        secMenu.classList.remove("hidden");
-        secCustomise.classList.add("hidden");
-        secInstall.classList.add("hidden");
-        secInfoText.textContent = "menu";
+        animateBtn.play();
 
-        btnContinue.classList.add("hidden");
+        setTimeout(() => {
+            secMenu.classList.remove("hidden");
+            secCustomise.classList.add("hidden");
+            secInstall.classList.add("hidden");
+            secInfoText.textContent = "menu";
+
+            btnContinue.classList.add("hidden");
+        }, 1000);
     });
     btnFinish.addEventListener("click", () => {
-        resetSite();
+        animateBtn.play();
 
-        showAlert("Congratulations! Your Device is ready! Press start to create another one!");
+        setTimeout(() => {
+            resetSite();
+            showAlert("Congratulations! Your Device is ready! Press start to create another one!");
+        }, 1000);
     });
 
 
@@ -304,6 +543,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-    // ——————————————————————————————————————————————————————————————————
+    btnOpenSidebar.addEventListener("click", () => {
+        animateSidebar.play();
+    });
+    btnCloseSidebar.addEventListener("click", () => {
+        playReversed(animateSidebar);
+    });
+
+
+
+    btnsChooseForm.forEach((btn, i) => {
+        btn.addEventListener("click", () => {
+            const form = document.querySelector(`.device__form${i + 1}`);
+            deviceForms.forEach(el => {
+                if (el == form) {
+                    el.classList.remove("hidden");
+                } else {
+                    el.classList.add("hidden");
+                }
+            });
+        }); 
+    });
+
+
+
+    // ON SITE LOAD ——————————————————————————————————————————————————————————————————
     showAlert("Press start to create your Witness!");
 });
